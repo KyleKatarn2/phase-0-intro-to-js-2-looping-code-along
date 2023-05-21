2a66eb1bebaa6ea80eea381785bc81335fdf0cce
Jonathan Campos <Malvyx5@gmail.com>
2023-05-21 13:22:33 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+}*/
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);*/
+
+
+function writeCards(array, string) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+
+    return newArray;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    let i = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+    
+    return gifts;
+}
+
+wrapGifts(gifts);*/
+
+function countDown() {
+    let count = 11;
+    while (count > 0) {
+        console.log(--count);
+    }
+}
